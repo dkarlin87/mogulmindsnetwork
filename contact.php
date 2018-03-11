bf5a6ad44ba39562a2c731dc1e2dd07e1b1813d7
Dan Karlin <dlkventuresllc@gmail.com>
2018-03-11 18:50:17 +0000

    added contact page functionality

---
+++ contact.php
@@ -0,0 +1,62 @@
+<?php
+/*
+ *  CONFIGURE EVERYTHING HERE
+ */
+
+// an email address that will be in the From field of the email.
+$from = 'Demo contact form <demo@domain.com>';
+
+// an email address that will receive the email with the output of the form
+$sendTo = 'Dan Karlin <dankarlin@soolmedia.com>';
+
+// subject of the email
+$subject = 'New message from contact form';
+
+// form field names and their translations.
+// array variable name => Text to appear in the email
+$fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message'); 
+
+
+error_reporting(E_ALL & ~E_NOTICE);
+
+try
+{
+
+    if(count($_POST) == 0) throw new \Exception('Form is empty');
+            
+    $emailText = "You have a new message from your contact form\n=============================\n";
+
+    foreach ($_POST as $key => $value) {
+        // If the field exists in the $fields array, include it in the email 
+        if (isset($fields[$key])) {
+            $emailText .= "$fields[$key]: $value\n";
+        }
+    }
+
+    $headers = array('Content-Type: text/plain; charset="UTF-8";',
+        'From: ' . $from,
+        'Reply-To: ' . $from,
+        'Return-Path: ' . $from,
+    );
+    
+    mail($sendTo, $subject, $emailText, implode("\n", $headers));
+
+    $responseArray = array('type' => 'success', 'message' => $okMessage);
+}
+catch (\Exception $e)
+{
+    $responseArray = array('type' => 'danger', 'message' => $errorMessage);
+}
+
+
+if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+    $encoded = json_encode($responseArray);
+
+    header('Content-Type: application/json');
+
+    echo $encoded;
+}
+// else just display the message
+else {
+    echo $responseArray['message'];
+}
--- script.js
+++ script.js
@@ -1,42 +1,33 @@
-// script.js
 
-    // create the module and name it scotchApp
-        // also include ngRoute for all our routing needs
-    var scotchApp = angular.module('scotchApp', ['ngRoute']);
+    var soolApp = angular.module('soolApp', ['ngRoute']);
 
-    // configure our routes
-    scotchApp.config(function($routeProvider) {
+    soolApp.config(function($routeProvider) {
         $routeProvider
 
-            // route for the home page
             .when('/', {
                 templateUrl : 'pages/home.html',
                 controller  : 'mainController'
             })
 
-            // route for the about page
-            .when('/about', {
-                templateUrl : 'pages/about.html',
-                controller  : 'aboutController'
+            .when('/projects', {
+                templateUrl : 'pages/projects.html',
+                controller  : 'projectsController'
             })
 
-            // route for the contact page
             .when('/contact', {
                 templateUrl : 'pages/contact.html',
                 controller  : 'contactController'
             });
     });
 
-    // create the controller and inject Angular's $scope
-    scotchApp.controller('mainController', function($scope) {
-        // create a message to display in our view
+    soolApp.controller('mainController', function($scope) {
         $scope.message = 'Everyone come and see how good I look!';
     });
 
-    scotchApp.controller('aboutController', function($scope) {
+    soolApp.controller('projectsController', function($scope) {
         $scope.message = 'Look! I am an about page.';
     });
 
-    scotchApp.controller('contactController', function($scope) {
+    soolApp.controller('contactController', function($scope) {
         $scope.message = 'Contact us! JK. This is just a demo.';
     });
